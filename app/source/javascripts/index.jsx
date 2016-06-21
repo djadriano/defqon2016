0bcb77c73fec81ec8fe9dd380d2e38d33c079903
Adriano Fernandes <adrianof@lt-r13-sp-005.ny.rga.com>
2016-06-21 18:04:32 -0300

    removed unused files

--- app/source/javascripts/index.jsx
+++ app/source/javascripts/index.jsx
@@ -6,10 +6,6 @@ import AppStyle from '../stylesheets/app';
 
 import LayoutApp from './pages/layout';
 import HomePage from './pages/home';
-import LoginPage from './pages/login';
-import SuccessPage from './pages/success';
-import LoggedPage from './pages/logged';
-import IpadPage from './pages/ipad';
 
 const app = document.getElementById('nklck');
 
@@ -17,10 +13,6 @@ ReactDom.render((
   <Router history={hashHistory}>
     <Route path="/" component={LayoutApp}>
       <IndexRoute component={HomePage} />
-      <Route path="login" component={LoginPage} />
-      <Route path="success" component={SuccessPage} />
-      <Route path="logged" component={LoggedPage} />
-      <Route path="ipad" component={IpadPage} />
     </Route>
   </Router>
 ), app);
